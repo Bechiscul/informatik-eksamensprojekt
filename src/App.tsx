8b86b7dfb03a52edc54d89ce7ea2f632291d76bd
Bech1337 <sebberas2@gmail.com>
2023-04-11 15:56:17 +0200

    negative and large numbers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import { jsPDF as PDF } from "jspdf";
 
 import Toolbar from "./Toolbar";
 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray<T>(array: T[]) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 type Task = {
   title: string;
   body: string;
@@ -15,19 +25,57 @@ type TaskItemProps = {
 };
 
 const TaskItem: Component<TaskItemProps> = (props) => {
-  const handleChange: JSX.EventHandler<HTMLTextAreaElement, Event> = (e) => {
+  const handleTitleChange: JSX.EventHandler<HTMLInputElement, Event> = (e) => {
+    props.updateTask({ ...props.task, title: e.currentTarget.value });
+  };
+
+  const handleBodyChange: JSX.EventHandler<HTMLTextAreaElement, Event> = (e) => {
     props.updateTask({ ...props.task, body: e.currentTarget.value });
+  };
+
+  const handleClick = () => {
+    let task: Task = {
+      title: props.task.title,
+      body: props.task.body,
+      questions: [...props.task.questions, ""],
+    };
+
+    props.updateTask(task);
+  };
+
+  const handleQuestionChange = (i: number) => {
+    const f: JSX.EventHandler<HTMLInputElement, Event> = (e) => {
+      let questions = props.task.questions;
+      questions[i] = e.currentTarget.value;
+      props.updateTask({ ...props.task, questions });
+    };
+
+    return f;
   };
 
   return (
     <li class="bg-white rounded-xl px-6 py-8 w-full">
-      <h2 class="text-2xl font-semibold">{props.task.title}</h2>
-      <textarea value={props.task.body} onchange={handleChange} class="w-full" />
-      <ol>
-        {props.task.questions.map((question) => (
-          <li>{question}</li>
+      <input
+        value={props.task.title}
+        onchange={handleTitleChange}
+        class="text-2xl font-semibold py-2 my-2"
+      />
+      <textarea value={props.task.body} onchange={handleBodyChange} class="w-full border p-2" />
+      <ol class="flex flex-col gap-2 pb-2">
+        {props.task.questions.map((question, i) => (
+          <li>
+            <input
+              type="text"
+              value={question}
+              onchange={handleQuestionChange(i)}
+              class="w-full border p-2"
+            />
+          </li>
         ))}
       </ol>
+      <button onclick={handleClick} class="px-4 text-2xl  bg-blue-400 text-white rounded">
+        +
+      </button>
     </li>
   );
 };
@@ -60,6 +108,7 @@ export type Cursor = { line: number; column: number } & (CursorAt | CursorSelect
 
 const App: Component = () => {
   const [cursor, setCursor] = createSignal<Cursor | null>(null);
+  const [generate, setGenerate] = createSignal<number>(1);
 
   const [title, setTitle] = createSignal("Opgavesæt");
   const [tasks, setTasks] = createSignal<Task[]>([
@@ -67,84 +116,102 @@ const App: Component = () => {
       title: "Opgave 1",
       body: "I disse opgave skal du anvende viden om ligninger til at løse ligninger samt redegøre for måden derpå",
       questions: [
-        "A) Løs følgende: \\[N1..5]\\x + 2 = 5",
+        "A) Løs følgende: \\[N100..500]\\x + 2 = 5",
         "B) Redegør for måden du løste delspørgsmål `A` på",
       ],
     },
   ]);
 
   const handleDownload = () => {
-    const document = new PDF();
-    const { getWidth, getHeight } = document.internal.pageSize;
-    const [marginX, marginY] = [25, 25];
+    for (let i = 0; i < generate(); i++) {
+      const document = new PDF();
+      const { getWidth, getHeight } = document.internal.pageSize;
+      const [marginX, marginY] = [25, 25];
 
-    // Front Page
-    document.setFontSize(32).text(title(), getWidth() / 2, 64, { align: "center" });
+      // Front Page
+      document.setFontSize(32).text(title(), getWidth() / 2, 64, { align: "center" });
 
-    for (const task of tasks()) {
-      document.addPage();
-      document.setFontSize(24).text(task.title, marginX, marginY + 0);
+      let randomTasks = [...tasks()];
+      shuffleArray(randomTasks);
+      console.log(randomTasks);
 
-      document.setFontSize(12).text(task.body, marginX, marginY + 12);
+      for (const task of randomTasks) {
+        document.addPage();
+        document.setFontSize(24).text(task.title, marginX, marginY + 0);
 
-      for (let i = 0; i < task.questions.length; i++) {
-        const question = task.questions[i];
+        document.setFontSize(12).text(task.body, marginX, marginY + 12);
 
-        if (question.includes("\\[") && question.includes("]\\")) {
-          // N5..7
-          const start = question.indexOf("\\[");
-          const end = question.lastIndexOf("]\\");
-          console.log(start);
-          console.log(end);
+        for (let i = 0; i < task.questions.length; i++) {
+          const question = task.questions[i];
 
-          const splitted = question.slice(start + 2, end);
-          console.log(splitted);
+          //
+          if (question.includes("\\[") && question.includes("]\\")) {
+            // Parses \\[N5..123]
+            const startExpression = question.indexOf("\\[");
+            const endExpression = question.lastIndexOf("]\\");
+            const expression = question.slice(startExpression + "\\[".length, endExpression);
+            // only natural numbers
+            if (expression.startsWith("N")) {
+              const startRange = expression.indexOf("..");
+              const endRange = startRange + "..".length;
+              Number.parseInt(expression.slice(1, startRange));
 
-          const min = Number.parseInt(splitted[1]);
-          const max = Number.parseInt(splitted[4]);
-          console.log(min);
-          console.log(max);
+              const min = Number.parseInt(expression.slice(1, startRange));
+              const max = Number.parseInt(expression.slice(endRange));
 
-          const number = Math.round(Math.random() * (max - min) + min);
-          const generatedQuestion = question
-            .slice(0, start)
-            .concat(`${number}`)
-            .concat(question.slice(end + 2));
+              const number = Math.round(Math.random() * (max - min) + min);
+              const generatedQuestion = question
+                .slice(0, startExpression)
+                .concat(`${number}`)
+                .concat(question.slice(endExpression + "]\\".length));
 
-          document.text(generatedQuestion, marginX, marginY + 24 + 12 * i);
-        } else {
-          document.text(question, marginX + 0, marginY + 24 + 12 * i);
+              document.text(generatedQuestion, marginX, marginY + 24 + 12 * i);
+            }
+          } else {
+            document.text(question, marginX + 0, marginY + 24 + 12 * i);
+          }
         }
       }
-    }
 
-    document.save();
+      document.save(`${i + 1}`);
+    }
   };
 
   const handleCreateTask = () => {
     setTasks((prev) =>
-      prev.concat([{ title: `Opgave ${prev.length + 1}`, body: "", questions: [] }])
+      prev.concat([
+        { title: `Opgave ${prev.length + 1}`, body: "Body", questions: ["A)", "B)", "C)"] },
+      ])
     );
   };
 
   return (
-    <div class="bg-[#D9D9D9] min-h-screen">
-      <header class="w-screen drop-shadow">
+    <div class="bg-[#D9D9D9] min-h-screen overflow-x-hidden pb-8">
+      <header class="w-screen drop-shadow sticky top-0 left-0">
         <nav class="w-full h-12 bg-[#002B59] flex items-center justify-center text-white">
           <Title title={title()} setTitle={setTitle} />
         </nav>
         <div class="w-full h-16 bg-white grid grid-cols-[1fr_auto] px-12 items-center">
           <Toolbar />
-          <button
-            onclick={handleDownload}
-            class="h-11 bg-blue-500 px-3 rounded-xl text-white font-medium"
-          >
-            Download
-          </button>
+          <div class="flex items-center gap-2">
+            <input
+              type="number"
+              min={1}
+              value={generate()}
+              onchange={(e) => setGenerate(Number.parseInt(e.currentTarget.value))}
+              class="w-12 h-10 border rounded text-center rounded-xl"
+            />
+            <button
+              onclick={handleDownload}
+              class="h-11 bg-blue-500 px-3 rounded-xl text-white font-medium"
+            >
+              Download
+            </button>
+          </div>
         </div>
       </header>
       <main class="px-36 flex items-center flex-col">
-        <ul class="w-full">
+        <ul class="w-full flex flex-col gap-8 py-8">
           {tasks().map((task, i) => (
             <TaskItem
               task={task}
